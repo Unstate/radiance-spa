fd13a1212336e80f0ab450c24de91b242179dd7b
Fetisov <rofetisov@mail.ru>
2024-02-18 21:34:02 +0300

    chore: add folder structure and delete useless files

---
+++ src/app/main.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import HomePage from '../pages/Home'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <HomePage />
+)
